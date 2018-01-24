8abd5a191e228f1152d3b75c592d151ec1735d34
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-01-25 04:03:18 +0900

    feat: Change Home component to display login button and obtained tokens

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,20 +1,32 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Home.css';
+// import { Link } from 'react-router-dom';
+// import styles from './Home.css';
 
-type Props = {};
+type Props = {
+  loading: boolean,
+  tokens: Object,
+  onLogin: SyntheticEvent => void,
+};
 
 export default class Home extends Component<Props> {
-  props: Props;
-
   render() {
     return (
       <div>
-        <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
-        </div>
+        {
+          this.props.tokens ?
+            <ul>
+              <li>Access Token: <span>{ this.props.tokens.access_token }</span></li>
+              <li>Refresh Token: <span>{ this.props.tokens.refresh_token }</span></li>
+              <li>Instance URL: <span>{ this.props.tokens.instance_url }</span></li>
+            </ul> :
+            <button
+              onClick={this.props.loading ? undefined : this.props.onLogin}
+              disabled={this.props.loading}
+            >
+                Login
+            </button>
+        }
       </div>
     );
   }
