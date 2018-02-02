11bd54380cab0e95b16e179819b66fc133d28f8f
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-02-03 02:06:22 +0900

    Delete unnecessary files

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,12 +1,10 @@
 // @flow
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
-import counter from './counter';
 import loading from './loading';
 import tokens from './tokens';
 
 const rootReducer = combineReducers({
-  counter,
   router,
   loading,
   tokens,
