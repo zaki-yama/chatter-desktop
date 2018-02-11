8671dae16b232d5d67a775465939d396d0803942
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-02-11 21:42:00 +0900

    Use react-lds button for login button

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 // import styles from './Home.css';
+import { Button } from 'react-lightning-design-system';
 
 type Props = {
   loading: boolean,
@@ -20,12 +21,13 @@ export default class Home extends Component<Props> {
               <li>Refresh Token: <span>{ this.props.tokens.refresh_token }</span></li>
               <li>Instance URL: <span>{ this.props.tokens.instance_url }</span></li>
             </ul> :
-            <button
+            <Button
+              type="neutral"
               onClick={this.props.loading ? undefined : this.props.onLogin}
               disabled={this.props.loading}
             >
                 Login
-            </button>
+            </Button>
         }
       </div>
     );
