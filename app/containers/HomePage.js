1ea7dad8f6e88a91094a0791fd0deabc13df8f74
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-02-24 17:49:55 +0900

    Save tokens locally

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,5 +1,6 @@
 // @flow
 import { connect } from 'react-redux';
+import storage from 'electron-json-storage';
 
 import Home from '../components/Home';
 import startAuth from '../utils/auth';
@@ -13,7 +14,31 @@ const mapDispatchToProps = dispatch => ({
   onClickLogin: async () => {
     dispatch({ type: 'LOGIN_START' });
     try {
-      const tokens = await startAuth();
+      console.log(storage.getDefaultDataPath());
+      let tokens = await new Promise((resolve, reject) => {
+        storage.get('tokens', (err, data) => {
+          if (err) {
+            console.log('err', err);
+            reject(err);
+          } else {
+            console.log('OK', data);
+            resolve(data);
+          }
+        });
+      });
+      if (Object.keys(tokens).length === 0) {
+        tokens = await startAuth();
+        await new Promise((resolve, reject) => {
+          storage.set('tokens', tokens, (err) => {
+            if (err) {
+              console.log('Failed to save tokens to localStorage', err);
+              reject(err);
+            }
+            console.log('Successfully save tokens to', storage.getDataPath(), tokens);
+            resolve();
+          });
+        });
+      }
       dispatch({ type: 'LOGIN_END' });
       dispatch({ type: 'SET_TOKENS', payload: { tokens } });
     } catch (e) {
