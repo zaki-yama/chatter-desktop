94088355e490a697a0c12caeead87091225d862f
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-03-24 01:49:06 +0900

    #10 Define MessageSegment type

---
+++ app/types/MessageSegment.js
@@ -0,0 +1,72 @@
+// @flow
+type messageSegment = Mention | Link | MarkupBegin | MarkupEnd;
+
+type Mention = {
+  type: 'Mention',
+  accessible: boolean,
+  name: string,
+  text: string,
+  record: Group | UserDetail | UserSummary,
+  user: UserSummary,
+};
+
+type Group = {
+  id: string,
+  type: 'CollaborationGroup',
+  myRole: | 'GroupOwner'
+    | 'GroupManager'
+    | 'NotAMember'
+    | 'NotAMemberPrivateRequested'
+    | 'StandardMember',
+  name: string,
+};
+
+type UserSummary = {
+  id: string,
+  type: 'User',
+  name: string,
+};
+
+type UserDetail = UserSummary;
+
+type Link = {
+  type: 'Link',
+  text: string,
+  reference: Reference,
+  url: string,
+};
+
+type Reference = {
+  id: string,
+  url: string,
+};
+
+type MarkupBegin = {
+  type: 'MarkupBegin',
+  htmlTag: string,
+  markupType: | 'Bold'
+    | 'Code'
+    | 'Italic'
+    | 'ListItem'
+    | 'OrderedList'
+    | 'Paragraph'
+    | 'Strikethrough'
+    | 'Underline'
+    | 'UnorderedList',
+  text: '',
+};
+
+type MarkupEnd = {
+  type: 'MarkupEnd',
+  htmlTag: string,
+  markupType: | 'Bold'
+    | 'Code'
+    | 'Italic'
+    | 'ListItem'
+    | 'OrderedList'
+    | 'Paragraph'
+    | 'Strikethrough'
+    | 'Underline'
+    | 'UnorderedList',
+  text: '',
+};
