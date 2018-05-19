21c5391d50a51d8db4feedf961366325f62cc221
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-05-20 00:43:40 +0900

    #10 Add utils/messageSegment (empty function)

--- app/types/MessageSegment.js
+++ app/types/MessageSegment.js
@@ -70,3 +70,5 @@ type MarkupEnd = {
     | 'UnorderedList',
   text: '',
 };
+
+export type MessageSegment;
---
+++ app/utils/messageSegment.js
@@ -0,0 +1,6 @@
+// @flow
+import { MessageSegment } from '../types/MessageSegment';
+
+export function parseMessageSegments(messageSegments: Array<MessageSegment>) {
+  return 'dummy';
+}
