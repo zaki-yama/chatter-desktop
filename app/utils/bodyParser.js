bf3e18651ac504a3ac8886813ae34b490f0afc1d
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-05-21 08:30:00 +0900

    #10 Implement bodyParser when isRichText=false

--- app/utils/bodyParser.js
+++ app/utils/bodyParser.js
@@ -1,7 +1,13 @@
 // @flow
+import type { MessageSegment } from '../types/MessageSegment';
 import type { FeedItemBody } from '../types/FeedItem';
 
 // eslint-disable-next-line import/prefer-default-export
 export function parseFeedItemBody(body: FeedItemBody) {
-  return body;
+  return body.isRichText
+    ? parseMessageSegments(body.messageSegments)
+    : body.text;
+}
+
+function parseMessageSegments(messageSegments: Array<MessageSegment>) {
 }
