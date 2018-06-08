82aa91393846cf7312865c00a5bdbe5fc73dc5d3
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-06-08 21:54:24 +0900

    #10 Always parse messageSegments

--- app/utils/bodyParser.js
+++ app/utils/bodyParser.js
@@ -4,13 +4,8 @@ import type { FeedItemBody } from '../types/FeedItem';
 
 // eslint-disable-next-line import/prefer-default-export
 export function parseFeedItemBody(body: FeedItemBody): string {
-  return body.isRichText
-    ? parseMessageSegments(body.messageSegments)
-    : body.text.replace(/u200B/g, '').replace(/\n/g, '<br>');
-}
-
-function parseMessageSegments(messageSegments: Array<MessageSegment>) {
-  return messageSegments.reduce((prev, current) => prev + parseMessageSegment(current), '');
+  // NOTE: Even isRichText = false, the text might contain mentions
+  return body.messageSegments.reduce((prev, current) => prev + parseMessageSegment(current), '');
 }
 
 function parseMessageSegment(messageSegment: MessageSegment): string {
